58fbf017ea2f39be56c5c35b4c5688a72ce5a087
pitzee <pitzee421@gmail.com>
2025-07-23 21:07:32 +0300

    Checks for data fetching

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -14,14 +14,14 @@ const Users: React.FC<UsersPageProps> = ({ users }) => {
       <h1 className="text-2xl font-bold mb-4">Users</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {users.map((user) => (
-          <UserCard key={user.email} {...user} />
+          <UserCard key={user.id} {...user} />
         ))}
       </div>
     </div>
   );
 };
 
-export const getStaticProps = async () => {
+export async function getStaticProps() {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await res.json();
 
@@ -30,6 +30,6 @@ export const getStaticProps = async () => {
       users: data,
     },
   };
-};
+}
 
 export default Users;
